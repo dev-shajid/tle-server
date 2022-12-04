284a1831c47b2143fd8ba3a9027af42c5c32f3b6
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2022-12-04 19:23:39 +0600

    TLE server init

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express")
-require("./db/connect")()
+// require("./db/connect")()
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
-app.use("/api", require('./routes/auth'))
+// app.use("/api", require('./routes/auth'))
 app.use("/api", require("./routes/duel"))
 
 app.listen(port, ()=> console.log(`Server is listening at http://localhost:${port}`))
